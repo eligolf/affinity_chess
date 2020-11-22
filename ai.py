4a30c7f7d9e3c8fb7c75e12a52b7b49bbdd6cc99
eligolf <59540119+eligolf@users.noreply.github.com>
2020-11-22 10:56:18 +0100

    Update ai.py

--- ai.py
+++ ai.py
@@ -18,8 +18,6 @@ class Ai:
     def __init__(self):
 
         self.killer_moves = {}
-        for depth in range(s.max_search_depth + 1):
-            self.killer_moves[depth] = []
 
         # Transposition table init
         self.tt_entry = {'value': 0, 'flag': '', 'depth': 0, 'best move': None, 'valid moves': []}
@@ -33,55 +31,72 @@ class Ai:
         self.opening_name = ''
         self.is_in_opening = True
 
-        self.not_PV = False
-
         self.counter = -1
+        self.nodes = {}
         self.check_mate_counter = 0
         self.is_check_counter = 0
         self.capture_counter = 0
+
+        self.best_moves = []
+
+        self.timer = 0
 
     def ai_make_move(self, gamestate, moves_list):
 
         # AI level 3, Negamax
         if s.level == 3:
 
+            # Init variables
+            for depth in range(gamestate.max_search_depth + 1):
+                self.killer_moves[depth] = []
+
+            self.nodes = {}
+
+            self.start_color = -1 if gamestate.is_ai_white else 1
+
             # Check if there is any opening moves to make
-            if self.is_in_opening and s.play_with_opening_book:
+            if self.is_in_opening and gamestate.play_with_opening_book and gamestate.start_fen == s.start_fen:
                 move = self.play_an_opening_move(gamestate, moves_list)
                 evaluation = 0
 
             # Normal Negamax if not in opening
-            if not self.is_in_opening or not s.play_with_opening_book:
+            if not self.is_in_opening or not gamestate.play_with_opening_book:
 
                 # Init parameters for iterative deepening
                 self.tt_entry = {'value': 0, 'flag': '', 'depth': 0, 'best move': None, 'valid moves': []}
+                self.best_moves = []
 
                 # Iterative deepening
                 time_start = time.time()
-                for depth in range(1, s.max_search_depth + 1):
+                for depth in range(1, gamestate.max_search_depth + 1):
 
-                    move, evaluation = self.negamax(gamestate, depth, -math.inf, math.inf, s.start_color)
+                    move, evaluation = self.negamax(gamestate, depth, -math.inf, math.inf, self.start_color)
+                    self.best_moves.append([move, evaluation])
 
                     time_end = time.time()
-                    timer = time_end - time_start
-                    print('---------------------------------------------')
-                    print('Depth: ', depth, '\nTime spent: ', str(round(timer, 2)) + ' s\n')
-                    print('Nodes searched: ' + str(self.counter))
-                    print('Check mates: ' + str(self.check_mate_counter))
-                    print('Checks: ' + str(self.is_check_counter))
-                    print('Captures: ' + str(self.capture_counter))
-                    print('---------------------------------------------')
+                    self.timer = time_end - time_start
+
+                    self.nodes[depth] = self.counter - sum(self.nodes.values())
+                    print('Depth: ', depth)
+                    #print('Nodes searched: ', self.nodes[depth])
+                    print('Time spent: ', round(self.timer, 2), 's\n')
+
                     self.counter = -1
-                    self.check_mate_counter = 0
-                    self.is_check_counter = 0
-                    self.capture_counter = 0
 
                     # Break if time has run our, if reached at least min depth, or if finding a mate in lowest number of moves
-                    if (timer > s.max_search_time and depth >= s.min_search_depth) or (evaluation / s.eval_level) > 100:
+                    if self.timer > s.max_search_time or depth >= s.min_search_depth or (evaluation / s.eval_level) > 100:
                         break
 
+                # Always return moves from an even number of depth, helps in some situation since quiescence search is not implemented
+                if len(self.best_moves) % 2 == 0:
+                    move = self.best_moves[-1][0]
+                    evaluation = self.best_moves[-1][1]
+                else:
+                    move = self.best_moves[-2][0]
+                    evaluation = self.best_moves[-2][1]
+
         # AI level 0, 1 or 2
-        elif s.level in [0, 1, 2]:
+        elif s.level in [0, 1]:
             levels = {0: self.ai_random(gamestate),
                       1: self.ai_1_ahead(gamestate),
                       2: self.ai_1_ahead(gamestate)}
@@ -97,9 +112,10 @@ class Ai:
 
     def negamax(self, gamestate, depth, alpha, beta, color):
         alpha_original = alpha
-        self.counter += 1
 
-        '''# Transposition table lookup (https://en.wikipedia.org/wiki/Negamax#Negamax_with_alpha_beta_pruning_and_transposition_tables)
+        #self.counter += 1
+
+        # Transposition table lookup (https://en.wikipedia.org/wiki/Negamax#Negamax_with_alpha_beta_pruning_and_transposition_tables)
         key = gamestate.zobrist_key
         if key in self.tt_entry and self.tt_entry[key]['depth'] >= depth:
             if self.tt_entry[key]['flag'] == 'exact':
@@ -109,27 +125,21 @@ class Ai:
             elif self.tt_entry[key]['flag'] == 'upperbound':
                 beta = min(beta, self.tt_entry[key]['value'])
             if alpha >= beta:
-                return self.tt_entry[key]['best move'], self.tt_entry[key]['value']'''
+                return self.tt_entry[key]['best move'], self.tt_entry[key]['value']
 
         # If depth is 0, always evaluate leaf node
         if depth == 0:
             return None, e.evaluate(gamestate, depth) * color
 
-        '''# Don't search valid moves again if it has been done in last iteration
+        # Don't search valid moves again if it has been done in last iteration
         if key in self.valid_moves_history and self.valid_moves_history[key]:
             children = self.valid_moves_history[key]
         else:
             children = gamestate.get_valid_moves()
-            self.valid_moves_history[key] = children'''
-        children = gamestate.get_valid_moves()
-
-        if gamestate.is_in_check:
-            self.is_check_counter += 1
+            self.valid_moves_history[key] = children
 
         # Need to calculate valid moves to know if there is a checkmate or stalemate
         if gamestate.is_check_mate or gamestate.is_stale_mate:
-            if gamestate.is_check_mate:
-                self.check_mate_counter += 1
             return None, e.evaluate(gamestate, depth) * color
 
         # Null move logic here (https://hci.iwr.uni-heidelberg.de/system/files/private/downloads/1935772097/report_qingyang-cao_enhanced-forward-pruning.pdf,
@@ -144,33 +154,32 @@ class Ai:
                     return None, evaluation'''
 
         # Sort moves before Negamax
-        #children = self.sort_moves(gamestate, children, depth, color)
+        children = self.sort_moves(gamestate, children, depth, color)
 
         # Negamax loop
         max_eval = -math.inf
         for child in reversed(children):
-            gamestate.make_move(child[0], child[1])
 
-            if gamestate.piece_captured != '--':
-                self.capture_counter += 1
-
+            gamestate.make_move(child[0], child[1], child[2])
             score = -self.negamax(gamestate, depth - 1, -beta, -alpha, -color)[1]
-
             gamestate.unmake_move()
 
             if score > max_eval:
                 max_eval = score
                 best_move = child
             alpha = max(alpha, max_eval)
-            '''if beta <= alpha:
+
+            # Beta cutoff
+            if beta <= alpha:
+
                 # Killer moves
                 if gamestate.piece_captured == '--':
                     self.killer_moves[depth].append(child)
-                    if len(self.killer_moves[depth]) == s.no_of_killer_moves:  # Keep killer moves at a maximum of 2 per depth
+                    if len(self.killer_moves[depth]) == s.no_of_killer_moves:  # Keep killer moves at a maximum of x per depth
                         self.killer_moves[depth].pop(0)
-                break'''
+                break
 
-        '''# Transposition table saving
+        # Transposition table saving
         self.tt_entry[gamestate.zobrist_key] = {'value': max_eval}
         if max_eval <= alpha_original:
             self.tt_entry[gamestate.zobrist_key]['flag'] = 'upperbound'
@@ -180,7 +189,7 @@ class Ai:
             self.tt_entry[gamestate.zobrist_key]['flag'] = 'exact'
 
         self.tt_entry[gamestate.zobrist_key]['depth'] = depth
-        self.tt_entry[gamestate.zobrist_key]['best move'] = best_move'''
+        self.tt_entry[gamestate.zobrist_key]['best move'] = best_move
 
         return best_move, max_eval
 
@@ -202,7 +211,7 @@ class Ai:
         for move in children:
             if gamestate.board[move[1]] != '--':
                 # If can capture last moved piece, then try this first
-                if move[1] == gamestate.move_log[-1][1]:
+                if move[1] == gamestate.move_log[-1][0][1]:
                     mvv_lva_values.append([s.mvv_lva_values[gamestate.board[move[1]][1]] - s.mvv_lva_values[gamestate.board[move[0]][1]] + 1000, move])
                 else:
                     mvv_lva_values.append([s.mvv_lva_values[gamestate.board[move[1]][1]] - s.mvv_lva_values[gamestate.board[move[0]][1]], move])
@@ -282,7 +291,7 @@ class Ai:
 
         for i, opening in enumerate(self.openings):
             if moves_list.lstrip() in opening:
-                partition = '.' if s.is_ai_white else ' '
+                partition = '.' if gamestate.is_ai_white else ' '
                 next_move = opening[len(moves_list.lstrip()) + 1:].partition(partition)[0]
                 opening_candidates.append((opening, next_move, i, self.opening_names[i]))
 
@@ -295,9 +304,9 @@ class Ai:
             if chosen_move_text:
                 # Castling
                 if chosen_move_text == 'O-O':
-                    return 25, 27
+                    return 25, 27, 'ck'
                 elif chosen_move_text == 'O-O-O':
-                    return 25, 23
+                    return 25, 23, 'cq'
 
                 # Find what start_square and end_square the chosen_move corresponds to, different if last move was a checking move.
                 if chosen_move_text[-1] == '+':
@@ -318,6 +327,6 @@ class Ai:
                             start_square = move[0]
                             break
 
-                return start_square, end_square
+                return start_square, end_square, 'no'
 
         self.is_in_opening = False
