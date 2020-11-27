b676af011c5e9f4935af261291bdc0ec2e8e611e
eligolf <59540119+eligolf@users.noreply.github.com>
2020-11-27 09:45:34 +0100

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
 # --------------------------------------------------------------------------------
 #                     GUI file running the game
 # --------------------------------------------------------------------------------
-import board as b
+import gamestate as gs
 import settings as s
 import ai
 import evaluation as e
@@ -10,6 +10,7 @@ import fen_handling as fh
 import PySimpleGUI as sg
 import cProfile
 import ctypes
+import re
 import os
 import sys
 import copy
@@ -37,11 +38,14 @@ class Gui:
         pygame.display.set_icon(s.icon)
 
         self.clock = pygame.time.Clock()
-        self.screen = pygame.display.set_mode((s.width, s.height))
+        self.screen = pygame.display.set_mode((s.win_width, s.win_height))
+
+        self.ai = ai.Ai()
 
         # Moves
-        self.moves_list = ''
+        self.moves_list = []
         self.latest_move = [(-100, -100, '')]
+        self.evaluation = '-'
 
         # Game parameters
         self.start_fen = s.start_fen
@@ -55,14 +59,14 @@ class Gui:
         # Start window to get user input for game settings
         self.is_started = False
         self.is_flipped = False
+        self.game_mode = 'human'
         self.draw('')
         sg.theme('DarkAmber')
         self.start_pop_up()
         self.is_started = True
 
         # Init Gamestate and AI
-        self.gamestate = b.GameState(self.start_fen, self.game_mode, self.is_ai_white, self.max_search_depth)
-        self.ai = ai.Ai()
+        self.gamestate = gs.GameState(self.start_fen, self.game_mode, self.is_ai_white, self.max_search_depth)
 
         # Flip board if AI is playing as white
         self.is_flipped = self.is_ai_white if self.game_mode == 'ai' else not self.is_white_turn
@@ -77,7 +81,7 @@ class Gui:
 
             self.clock.tick(s.fps)
 
-            self.square_under_mouse = self.get_square_under_mouse()
+            self.square_under_mouse, col, row = self.get_square_under_mouse()
 
             # Events happening in GUI
             for event in pygame.event.get():
@@ -91,18 +95,30 @@ class Gui:
 
                 # Mouse events
                 elif event.type == pygame.MOUSEBUTTONDOWN:
+                    pos = pygame.mouse.get_pos()
+
                     if event.button == 1:
                         self.is_dragging = True
-                        if self.gamestate.board[self.square_under_mouse][0] == ('w' if self.gamestate.is_white_turn else 'b'):
+                        if self.gamestate.board[self.square_under_mouse][0] == ('w' if self.gamestate.is_white_turn else 'b') and 1 <= col <= 8 and 2 <= row <= 9:
                             self.selected_square = self.square_under_mouse
                         else:
                             self.selected_square = 0
                         self.x, self.y = pygame.mouse.get_pos()[0] - s.sq_size // 2, pygame.mouse.get_pos()[1] - s.sq_size // 2
 
+                    # Button clicks
+                    if self.undo_button.collidepoint(pos):
+                        self.unmake_a_move()
+
+                    if self.restart_button.collidepoint(pos):
+                        self.restart_game()
+
+                    if self.flip_button.collidepoint(pos):
+                        self.is_flipped = not self.is_flipped
+
                 elif event.type == pygame.MOUSEBUTTONUP:
                     if event.button == 1:
                         self.is_dragging = False
-                        move = (self.selected_square, self.get_square_under_mouse())
+                        move = (self.selected_square, self.get_square_under_mouse()[0])
                         valid_moves = self.gamestate.get_valid_moves()
                         for possible_move in valid_moves:
 
@@ -123,23 +139,21 @@ class Gui:
                 # Keyboard events
                 elif event.type == pygame.KEYDOWN:
 
-                    # Unmake move by pressing 'z'-key, only if not being in start position
+                    # Unmake move by pressing 'z'-key
                     if event.key == pygame.K_z:
-                        if len(self.gamestate.move_log) > 1:
-                            self.unmake_a_move()
+                        self.unmake_a_move()
 
                     # Restart game with 'r'-key
                     elif event.key == pygame.K_r:
-                        event = pop_up('Restart game', 'Are you sure you want to restart?', True)
-                        if event == 'Yes':
-                            Gui().main()
+                        self.restart_game()
 
                     # Flip screen with 'f'-key
                     elif event.key == pygame.K_f:
                         self.is_flipped = not self.is_flipped
 
             # Draw the screen after human move
-            self.draw(self.gamestate.get_valid_moves())
+            if running:
+                self.draw(self.gamestate.get_valid_moves())
 
             # Check if stalemated/checkmated and stop the game if so
             if self.gamestate.is_check_mate or self.gamestate.is_stale_mate:
@@ -148,9 +162,9 @@ class Gui:
 
             # If move made and game not over, change to AI if that option is chosen.
             if (self.move_made, running, self.game_mode) == (True, True, 'ai'):
-                move, evaluation = self.ai.ai_make_move(self.gamestate)
+                move, self.evaluation = self.ai.ai_make_move(self.gamestate)
                 self.process_move(move)
-                self.print_eval(evaluation)
+                self.process_eval()
 
 # --------------------------------------------------------------------------------
 #                      Draw everything in GUI
@@ -158,16 +172,51 @@ class Gui:
 
     def draw(self, valid_moves):
 
-        # Draw the board
+        # Background and board edge
+        self.screen.blit(s.bg, (0, 0))
+        self.screen.blit(s.board_edge, (s.board_offset - 4, s.board_offset - 4))
+
+        # Board
+        self.draw_board(valid_moves)
+
+        # Info surface to the right of board with text
+        self.draw_info_surface()
+
+        # Buttons
+        self.draw_buttons()
+
+        # Update screen
+        pygame.display.flip()
+
+    def draw_info_surface(self):
+
+        # Background
+        if self.game_mode == 'ai':
+            pygame.draw.rect(self.screen, s.info_surface_color, (s.width + s.sq_size, s.board_offset - 4, s.info_width, s.info_height))
+
+        # Text on surface
+        self.draw_text()
+
+    def draw_board(self, valid_moves):
+
+        # Letters and numbers
+        for char in range(8):
+            real_char = 7 - char if self.is_flipped else char
+            self.create_text(s.board_letters[char], s.board_font, s.gold, s.board_offset + 0.5*s.sq_size + real_char*s.sq_size, s.win_height - 0.5*s.board_offset, True)
+            self.create_text(s.board_numbers[char], s.board_font, s.gold, 0.5*s.board_offset, s.board_offset + 0.5*s.sq_size + s.sq_size*real_char, True)
+
+        # Board
+        real_square = 0
         for row in range(12):
             for col in range(10):
                 if 2 <= row <= 9 and 1 <= col <= 8:
-                    x, y, w = (s.sq_size * (col - 1), s.sq_size * (row - 2), s.sq_size) if not self.is_flipped \
-                         else (s.sq_size * (8 - col), s.sq_size * (9 - row), s.sq_size)
+                    x, y, w = (s.sq_size * (col - 1) + s.board_offset, s.sq_size * (row - 2) + s.board_offset, s.sq_size) if not self.is_flipped \
+                         else (s.sq_size * (8 - col) + s.board_offset, s.sq_size * (9 - row) + s.board_offset, s.sq_size)
 
                     # Squares
-                    square_color = s.board_colors[(row + col) % 2]
-                    pygame.draw.rect(self.screen, square_color, pygame.Rect(x, y, w, w))
+                    square = s.dark_square[real_square] if (row + col) % 2 == 0 else s.light_square[real_square]
+                    self.screen.blit(square, pygame.Rect(x, y, w, w))
+                    real_square += 1
 
                     # Highlight squares if game is started
                     if self.is_started:
@@ -186,24 +235,21 @@ class Gui:
                 piece = self.gamestate.board[self.selected_square]
                 self.screen.blit(s.images[piece], pygame.Rect(self.x, self.y, s.sq_size, s.sq_size))
 
-        # Update screen
-        pygame.display.flip()
-
     def highlight_squares(self, valid_moves):
 
         # Highlight if king is in check
         if self.gamestate.is_in_check:
             king_pos = self.gamestate.white_king_location if self.gamestate.is_white_turn else self.gamestate.black_king_location
             row, col = (king_pos // 10 - 2, king_pos % 10 - 1) if not self.is_flipped else (9 - king_pos // 10, 8 - king_pos % 10)
-            pygame.draw.rect(self.screen, s.check_red, (col * s.sq_size, row * s.sq_size, s.sq_size - 1, s.sq_size - 1), 3)
+            pygame.draw.rect(self.screen, s.check_red, (col * s.sq_size + s.board_offset, row * s.sq_size + s.board_offset, s.sq_size, s.sq_size), 2)
 
         # Highlight latest move
         start_row, start_col = (self.latest_move[-1][0] // 10 - 2, self.latest_move[-1][0] % 10 - 1) if not self.is_flipped else \
                                (9 - self.latest_move[-1][0] // 10, 8 - self.latest_move[-1][0] % 10)
         end_row, end_col = (self.latest_move[-1][1] // 10 - 2, self.latest_move[-1][1] % 10 - 1) if not self.is_flipped else \
                            (9 - self.latest_move[-1][1] // 10, 8 - self.latest_move[-1][1] % 10)
-        pygame.draw.rect(self.screen, s.grey[3], (start_col * s.sq_size, start_row * s.sq_size, s.sq_size - 1, s.sq_size - 1), 4)
-        pygame.draw.rect(self.screen, s.grey[3], (end_col * s.sq_size, end_row * s.sq_size, s.sq_size - 1, s.sq_size - 1), 4)
+        pygame.draw.rect(self.screen, s.grey[1], (start_col * s.sq_size + s.board_offset, start_row * s.sq_size + s.board_offset, s.sq_size, s.sq_size), 3)
+        pygame.draw.rect(self.screen, s.grey[1], (end_col * s.sq_size + s.board_offset, end_row * s.sq_size + s.board_offset, s.sq_size, s.sq_size), 3)
 
         # Highlight when dragging a piece
         if self.is_dragging:
@@ -213,33 +259,61 @@ class Gui:
 
             # Selected square
             if self.gamestate.board[square][0] == ('w' if self.gamestate.is_white_turn else 'b'):
-                pygame.draw.rect(self.screen, s.orange, (col * s.sq_size, row * s.sq_size, s.sq_size - 2, s.sq_size - 2), 5)
+                pygame.draw.rect(self.screen, s.orange, (col * s.sq_size + s.board_offset, row * s.sq_size + s.board_offset, s.sq_size, s.sq_size), 4)
 
             # Possible moves
             for move in valid_moves:
                 if move[0] == square:
                     row, col = (move[1] // 10 - 2, move[1] % 10 - 1) if not self.is_flipped else \
                                (9 - move[1] // 10, 8 - move[1] % 10)
-                    pygame.draw.rect(self.screen, s.green, (col * s.sq_size, row * s.sq_size, s.sq_size - 2, s.sq_size - 2), 4)
+                    pygame.draw.rect(self.screen, s.green, (col * s.sq_size + s.board_offset, row * s.sq_size + s.board_offset, s.sq_size, s.sq_size), 4)
 
-    def print_eval(self, evaluation):
-        evaluation = evaluation
+    def create_buttons(self, text, font, color, x, y):
+        text = font.render(text, True, color)
+        rect = text.get_rect()
+        rect.center = (x, y)
+
+        box = (rect[0] - s.sq_size/10, rect[1] - s.sq_size/10 / 2, rect[2] + s.sq_size/10 * 2, rect[3] + s.sq_size/10)
+        pygame.draw.rect(self.screen, s.info_surface_color, box, int(s.sq_size/20 / 2))
+        self.screen.blit(text, rect)
+
+        return text, rect
+
+    def create_text(self, text, font, color, x, y, center):
+        text = font.render(text, True, color)
+        rect = text.get_rect()
+        if center:
+            rect.center = (x, y)
+        else:
+            rect.midleft = (x, y)
+        self.screen.blit(text, rect)
+
+    def draw_buttons(self):
+
+        self.restart_text, self.restart_button = self.create_buttons('Restart', s.large_font, s.large_color, 1.25*s.width, 0.86*s.height)
+        self.flip_text, self.flip_button = self.create_buttons('Flip Board', s.large_font, s.large_color, 1.25*s.width, 0.93*s.height)
+        self.undo_text, self.undo_button = self.create_buttons('Undo move', s.large_font, s.large_color, 1.25*s.width, s.height)
+
+    def draw_text(self):
+
+        # AI-related prints
+        if self.game_mode == 'ai':
+            self.create_text('AI info', s.title_font, s.black, 1.14*s.width*1.05, 0.75*s.sq_size, False)
+            self.create_text(f'Level: {s.level[self.max_search_depth]}', s.info_font, s.black, 1.14*s.width, 0.75*1.4*s.sq_size, False)
+            self.create_text(f'Depth: {self.ai.max_depth}', s.info_font, s.black, 1.14*s.width, 0.75 * 1.8 * s.sq_size, False)
+            self.create_text(f'Eval: {self.evaluation}', s.info_font, s.black, 1.14*s.width, 0.75*2.2*s.sq_size, False)
+
+    def process_eval(self):
 
         if self.ai.is_in_opening:
-            evaluation = '0.00 (Opening)'
+            self.evaluation = 'Opening'
         else:
-            if evaluation > 1e6:
-                evaluation = 'AI wins'
-            elif evaluation < -1e6:
-                evaluation = 'Human wins'
+            if self.evaluation > 1e6:
+                self.evaluation = 'AI wins'
+            elif self.evaluation < -1e6:
+                self.evaluation = 'Human wins'
             else:
-                evaluation = round(evaluation / 100, 2)
-
-        print('-------------------------')
-        print(self.moves_list)
-        print('')
-        print('Evaluation: ' + str(evaluation))
-        print('-------------------------\n')
+                self.evaluation = round(self.evaluation / 100, 2)
 
 # --------------------------------------------------------------------------------
 #               Helper functions
@@ -263,23 +337,34 @@ class Gui:
             evaluation = e.evaluate(self.gamestate)
             print('(Static evaluation for ' + ('white: ' if not self.gamestate.is_white_turn else 'black: ') + str(evaluation if self.gamestate.is_white_turn else -evaluation) + ')')
 
+        # Flip board if human vs human and if board is not already flipped in the right direction for player turn
+        if self.game_mode == 'human':
+            if (self.gamestate.is_white_turn and self.is_flipped) or (not self.gamestate.is_white_turn and not self.is_flipped):
+                self.is_flipped = not self.is_flipped
+
         # Change move made variable
         self.move_made = not self.move_made
 
-        # Flip board if human vs human
-        if self.game_mode == 'human':
-            self.is_flipped = not self.is_flipped
-            print(self.moves_list)
-
     def unmake_a_move(self):
-        self.gamestate.move_counter -= 0.5
-        self.moves_list = self.moves_list.rsplit(' ', 1)[0].rstrip()
+        if len(self.gamestate.move_log) > 1:
 
-        self.gamestate.unmake_move()
-        self.latest_move.pop()
-        if self.game_mode == 'human':
-            self.is_flipped = not self.is_flipped
-            print(self.moves_list)
+            # Unmake twice if playing against the AI
+            undo_move = 1 if self.game_mode == 'human' else 2
+
+            for undo in range(0, undo_move):
+                self.gamestate.move_counter -= 0.5
+                self.moves_list.pop()
+
+                self.gamestate.unmake_move()
+                self.latest_move.pop()
+                if self.game_mode == 'human':
+                    if (self.gamestate.is_white_turn and self.is_flipped) or (not self.gamestate.is_white_turn and not self.is_flipped):
+                        self.is_flipped = not self.is_flipped
+
+    def restart_game(self):
+        event = pop_up('Restart game', 'Are you sure you want to restart?', True)
+        if event == 'Yes':
+            Gui().main()
 
     def add_move_to_list(self, start_square, end_square):
 
@@ -290,7 +375,7 @@ class Gui:
         else:
             move_text = ' ' + text
 
-        self.moves_list += move_text
+        self.moves_list.append(move_text)
 
     def return_move_as_string(self, start_square, end_square):
 
@@ -303,10 +388,11 @@ class Gui:
 
         # The piece that is moving
         piece = self.gamestate.piece_moved[1]
+
+        # If same type of piece can reach same square, add some extra info
         letter = number = False
         extra_info = ''
 
-        # If same type of piece can reach same square
         if piece != 'p':
             for move in self.gamestate.possible_moves:
                 if move[1] == end_square and piece == self.gamestate.board[move[0]][1]:
@@ -355,11 +441,12 @@ class Gui:
 
     def get_square_under_mouse(self):
         pos = pygame.mouse.get_pos()  # (x, y) location of click
-        col, row = ((pos[0] + s.sq_size) // s.sq_size, (pos[1] + 2 * s.sq_size) // s.sq_size) if not self.is_flipped \
-            else (9 - (pos[0] + s.sq_size) // s.sq_size, 11 - (pos[1] + 2 * s.sq_size) // s.sq_size)  # Get corresponding square in chess board
+        col, row = ((pos[0] + s.sq_size - s.board_offset) // s.sq_size, (pos[1] + 2 * s.sq_size - s.board_offset) // s.sq_size) if not self.is_flipped \
+            else (9 - (pos[0] + s.sq_size - s.board_offset) // s.sq_size, 11 - (pos[1] + 2 * s.sq_size - s.board_offset) // s.sq_size)  # Get corresponding square in chess board
+
         square_under_mouse = row * 10 + col  # Get corresponding square on the 10x12 board
 
-        return square_under_mouse
+        return square_under_mouse, col, row
 
     def chose_promotion_piece(self, possible_move):
         event, values = sg.Window('', [[sg.Text('Promote to:')],
